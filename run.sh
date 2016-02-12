50ccb8c835b0d277b7f2498664770322bedb6d9e
Ricardo Carrillo Cruz <ricardo.carrillo.cruz@gmail.com>
2016-02-12 13:54:15 +0100

    Create and attach other required openstack resources
    Rather than expecting a user to create keypair prior to run
    infra-ansible, just make infra-ansible to create the keypair
    per the variable passed as keypair_contents on infra_config.yml.
    Role provision_infra_servers not only creates instances, but also
    volumes and now kepairs, thus also renaming the provision_infra_servers
    role and playbook to setup_openstack_resources.
    
    Change-Id: I810edf1b8956d2f5653aa789ce908428c2904549

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ansible-playbook -i hosts provision_infra_servers.yml -e "@infra_config.yml"
+ansible-playbook -i hosts setup_openstack_resources.yml -e "@infra_config.yml"
 ansible-playbook -i inventory/openstack.py site.yml -e "@infra_config.yml"
